239e60048b4a89f467250f9ba2b8716f03319640
Rajrohith <rajrohith902@gmail.com>
2025-05-10 21:29:07 +0530

    port1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import MainPage from './pages/mainpage'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 function App() {
  
 
   return (
-     <BrowserRouter>
+     
      <Routes>
       <Route path='/' element={<MainPage/>}></Route>
      </Routes>
-     </BrowserRouter>
   )
 }
 
